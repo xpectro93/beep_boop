9af5aaa62dbbd38cec7a44c54c038518cfc2f0c6
Jonathan <jonanath@hotmail.com>
2020-11-02 18:59:03 -0500

    add bg image to banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Menu from './components/Menu.js'
 import Footer from "./components/Footer.js";
-
+import Banner from './components/Banner.js';
 function App() {
   return (
     <div className="App">
       <div className="background-static"></div>
       <div className="main-wrapper">
+        <Banner/>
         <Menu/>
       </div>
       <Footer/>
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import "../CSS/Banner.css"
 export default function Banner () {
     return (
         <div id="main-banner">
             <div id="me"></div>
-            
+            <div id="title-container">
+                <h1>Jonathan</h1>
+                <h1>Andrade</h1>
+                <h2>FullStack Web Developer</h2>
+            </div>
         </div>
     )
 }
