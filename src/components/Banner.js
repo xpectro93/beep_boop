dccf85d638c6f7528462fa4e55a4dee53695a4b2
Jonathan <jonanath@hotmail.com>
2020-11-02 18:38:57 -0500

    add Banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Banner () {
+    return (
+        <div id="main-banner">
+            <div id="me"></div>
+            
+        </div>
+    )
+}
