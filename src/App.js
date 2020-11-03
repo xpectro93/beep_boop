e03851766ef2581c663e6c81e968311f7d1774cb
xpextro93 <jonathanandrade@pursuit.org>
2020-11-03 18:04:13 -0500

    import AnimatePresence into app

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import Menu from './components/Menu.js'
 import Footer from "./components/Footer.js";
 import Banner from './components/Banner.js';
+
+import { AnimatePresence } from "framer-motion";
+
 function App() {
   return (
     <div className="App">
       <div className="background-static"></div>
-      <div className="main-wrapper">
-        <Banner/>
-        <Menu/>
-      </div>
-      <Footer/>
+      <AnimatePresence initial={true} exitBeforeEnter>
+        <div className="main-wrapper">
+          <Banner/>
+          <Menu/>
+        </div>
+        <Footer/>
+      </AnimatePresence>
     </div>
   );
 }
