29d745c4c6c730a17b4bb2e8b45c269bb492d103
xpextro93 <jonathanandrade@pursuit.org>
2020-11-07 21:20:05 -0500

    import nes css to about me page

--- src/components/AbouMe.js
+++ src/components/AbouMe.js
@@ -1,9 +1,9 @@
 import "../CSS/AboutMe.css"
-
+import "nes.css/css/nes.min.css";
 export default function AboutMe () {
 
     return(
-        <div className ="about-me">
+        <div className ="about-me nes-container is-rounded is-dark">
             "This is my about me, being about me"
         </div>
     )
