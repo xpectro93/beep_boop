442bfedcc99b11706c628abbf7e730fcd3fd26c1
xpextro93 <jonathanandrade@pursuit.org>
2020-11-07 21:26:19 -0500

    add first version for modal

--- src/components/AbouMe.js
+++ src/components/AbouMe.js
@@ -3,8 +3,8 @@ import "nes.css/css/nes.min.css";
 export default function AboutMe () {
 
     return(
-        <div className ="about-me nes-container is-rounded is-dark">
-            "This is my about me, being about me"
+        <div className ="about-me">
+           <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
         </div>
     )
 };
