b926d194ec5ef6ef0cbcdd8126b64ab6029c2907
xpextro93 <jonathanandrade@pursuit.org>
2020-11-17 22:31:02 -0500

    add function to import all images from a directory

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,5 +1,15 @@
-import "../index.css"
+import "../CSS/Technologies.css"
 
+// import all images from the technologies subdirectory in assets
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+  
+const images = importAll(require.context('../assets/technologies', false, /\.(png|jpe?g|svg)$/));
+  
 export default function Technologies() {
-    return <div className="techs"></div>;
+    console.log(images)
+    return <div className="technologies"><img src={images["html.png"].default} /></div>;
 }
