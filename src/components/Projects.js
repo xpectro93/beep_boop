3db1f14b2801ed5acae527966760af7262ed4bf0
xpextro93 <jonathanandrade@pursuit.org>
2020-11-18 23:55:14 -0500

    add project array

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,41 @@
+import "../CSS/Projects.css"
+
+const projects = [
+    {
+      title:"Envizo",
+      text:"Blah environment blah",
+      image:"www.image.com",
+      github:"https://github.com/xpectro93/myEnvizo",
+      live:"https://myenvizo.herokuapp.com/"
+    },
+    {
+      title:"Bluebadger",
+      text:"I hate my life",
+      image:"sad image",
+      github:"https://github.com/xpectro93/BB-Den",
+      live:"http://bluebadger.army/"
+    },
+    {
+      title:"Stonks",
+      text:"Meme profits are going up",
+      image:"www.sadmage.com",
+      github:"https://github.com/xpectro93/TTP_Fullstack",
+      live:"http://ttp-fs-jonathanandrade.herokuapp.com/"
+    },
+    {
+      title:"Pathfinder",
+      text:"Finds the path",
+      image:"www.path.com",
+      github:"https://github.com/xpectro93/Pathfinder",
+      live:"https://xpectro93.github.io/Pathfinder"
+    },
+]
 
 
 export default function Projects () {
-    return "Yo look at all the things i've done"
+    return (
+        <div className="projects overflow">
+
+        </div>
+    )
 }
