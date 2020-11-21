e2b9640701dbb9bd424e6df29dc696077167d923
xpextro93 <jonathanandrade@pursuit.org>
2020-11-20 19:48:59 -0500

    update content css and modal js css

--- src/Modal.js
+++ src/Modal.js
@@ -2,7 +2,7 @@ import "./CSS/Modal.css"
 import "nes.css/css/nes.min.css";
 import { motion }  from 'framer-motion'; 
 
-const buttonCss ={background:"white",width:"50%",height:"100px",margin:"1vh auto"}
+const buttonCss ={background:"white",width:"50%",height:"50px",margin:"1vh auto"}
 
 export default function Modal (props) {
     return(
