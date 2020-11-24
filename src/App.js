6da8bfc3b453a2d291abfc1f50c8d496901b47dd
xpextro93 <jonathanandrade@pursuit.org>
2020-11-24 13:53:45 -0500

    add router 404 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import Content from './components/Content.js'
 import Banner from './components/Banner.js';
+import { Route, Switch } from "react-router-dom";
 
 import { AnimatePresence } from "framer-motion";
 
@@ -11,14 +12,23 @@ function App() {
     <div className="App">
       <div className="background-static"></div>
       <AnimatePresence initial={true} exitBeforeEnter>
-
-        
         <div className="main-wrapper" key={"main-wrapper"}>
           <Banner/>
-          <Content/>
-        </div>
 
-        
+          <Switch>
+          <Route exact path="/">
+           <Content/>
+          </Route>
+          <Route path="*">
+            <div style={{color:"white",margin:"auto"}}>
+              ERROR ERROR BOOP BOOP
+            </div>
+          </Route>
+
+
+
+          </Switch>
+        </div> 
       </AnimatePresence>
       
     </div>
