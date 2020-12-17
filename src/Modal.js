d8a604c02feaaa01b93ee203ac9e421bd248afa0
xpextro93 <jonathanandrade@pursuit.org>
2020-12-17 15:14:35 -0500

    upddate font size on modal box

--- src/Modal.js
+++ src/Modal.js
@@ -2,7 +2,7 @@ import "./CSS/Modal.css"
 import "nes.css/css/nes.min.css";
 import { motion }  from 'framer-motion'; 
 
-const buttonCss ={background:"white",width:"50%",margin:"1vh auto", height:"min-content"}
+const buttonCss ={background:"white",width:"50%",margin:"1vh auto", height:"min-content", fontSize:"15px"}
 
 export default function Modal (props) {
     return(
