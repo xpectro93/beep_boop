4f70ce50606a8c62118d1b28be7140d15f7d4703
xpextro93 <jonathanandrade@pursuit.org>
2020-12-17 15:37:34 -0500

    attempt to transferm css to css sheet

--- src/Modal.js
+++ src/Modal.js
@@ -1,5 +1,5 @@
 import "./CSS/Modal.css"
-import "nes.css/css/nes.min.css";
+// import "nes.css/css/nes.min.css";
 import { motion }  from 'framer-motion'; 
 
 const buttonCss ={background:"white",width:"50%",margin:"1vh auto", height:"min-content", fontSize:"15px"}
@@ -13,7 +13,8 @@ export default function Modal (props) {
             
             className ="le-modal">
            {props.children}
-           <button style={buttonCss} onClick={()=>props.select('')}>Close</button>
+           <button className="modal-button" onClick={()=>props.select('')}>Close</button>
         </motion.div>
     )
 };
+// style={buttonCss} 
