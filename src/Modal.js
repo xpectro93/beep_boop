51afdc0cf92ac41df018676da70d8a48b8787755
xpextro93 <jonathanandrade@pursuit.org>
2020-12-17 15:06:54 -0500

    update height on modal button to attemp fix on iphone

--- src/Modal.js
+++ src/Modal.js
@@ -2,7 +2,7 @@ import "./CSS/Modal.css"
 import "nes.css/css/nes.min.css";
 import { motion }  from 'framer-motion'; 
 
-const buttonCss ={background:"white",width:"50%",margin:"1vh auto"}
+const buttonCss ={background:"white",width:"50%",margin:"1vh auto,", height:"10%"}
 
 export default function Modal (props) {
     return(
